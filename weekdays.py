67a8c1170b7c304c68c70589a353c550ba3162dc
sharmaatul9654 <60250341+sharmaatul9654@users.noreply.github.com>
2020-02-14 11:02:26 +0530

    weekdays

---
+++ weekdays.py
@@ -0,0 +1,18 @@
+a=int(input())
+if(a%7==0):
+ print("sunday")
+elif(a%7==1):
+ print("monday")
+elif(a%7==2):
+ print("tuesday")
+elif(a%7==3):
+ print("wednesday")
+elif(a%7==4):
+ print("thursday")
+elif(a%7==5):
+ print("friday")
+elif(a%7==6):
+ print("saturday")
+else:
+print('number is not defined')
+
